75fb71e7eb04b8a51d3b881bc963cedfdebbe5d5
Wanderson <wa4d3rso4@gmail.com>
2025-07-24 23:16:20 -0300

    criando projeto

---
+++ src/test/java/com/wanderson/Desafio_03/Desafio03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderson.Desafio_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
